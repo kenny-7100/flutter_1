8c892210650e394320f241c21c9701ee2c0a37da
jimao <jimao@jimaodeMac-mini-2.local>
2026-02-15 19:13:01 +0800

    feat: change title

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter 1 App',
       theme: ThemeData(
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter 1'),
     );
   }
 }
