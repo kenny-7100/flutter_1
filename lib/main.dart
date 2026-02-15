47f407c7acc5e1e95f6575fb1cc421a6f370ccb8
jimao <jimao@jimaodeMac-mini-2.local>
2026-02-15 20:08:34 +0800

    feat: auto format

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_1/slippage_selector.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,10 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
         textTheme: const TextTheme(
-          bodyMedium: TextStyle(
-            color: Colors.white,
-            fontSize: 14,
-          ),
+          bodyMedium: TextStyle(color: Colors.white, fontSize: 14),
         ),
       ),
       home: const MyHomePage(title: 'Flutter 1'),
@@ -49,21 +47,12 @@ class _MyHomePageState extends State<MyHomePage> {
       backgroundColor: Colors.black,
       appBar: AppBar(
         backgroundColor: Colors.white,
-        title: Text(widget.title, style: TextStyle(fontSize: 20, color: Colors.black)),
+        title: Text(
+          widget.title,
+          style: TextStyle(fontSize: 20, color: Colors.black),
+        ),
       ),
-      body: Container(
-        color: Colors.black,
-        width: double.infinity,
-        height: 100,
-        // margin: EdgeInsets.all(16),
-        // padding: EdgeInsets.all(10),
-        // decoration: BoxDecoration(
-        //   color: Color(0xffeeeeee),
-        //   border: Border.all(color: Colors.black, width: 1),
-        //   borderRadius: BorderRadius.circular(10),
-        // ),
-        child: Text('你好，世界$_counter'),
-      ),
+      body: SlippageSelector(),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
---
+++ lib/slippage_selector.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SlippageSelector extends StatelessWidget {
+  const SlippageSelector({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        children: [
+          Row(children: [Text('Slippage Tolerance')]),
+        ],
+      ),
+    );
+  }
+}
