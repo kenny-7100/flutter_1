aa2b94161498af9a6c07a775dea0ce1aa81333de
jimao <jimao@jimaodeMac-mini-2.local>
2026-02-15 19:35:36 +0800

    feat: backgroundColor

--- lib/main.dart
+++ lib/main.dart
@@ -40,6 +40,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
